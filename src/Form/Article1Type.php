c782c3750e4fe4c21462d32b2bd6a35fca6184c9
Hristina <hristinastankova89@gmail.com>
2023-11-26 20:30:00 +0100

    adding CRUD

--- src/Form/Article1Type.php
+++ src/Form/Article1Type.php
@@ -3,28 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Article;
-use App\Entity\Category;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ArticleType extends AbstractType
+class Article1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title',TextType::class, [
-                'label' => "Titre de l'article"
-            ])
+            ->add('title')
+            ->add('createdOn')
             ->add('visible')
-            ->add('category', EntityType::class, [
-'class' => Category::class,
-'choice_label' => 'name'
-            ])
-        ->add("Save", SubmitType::class, []);
+            ->add('category')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
